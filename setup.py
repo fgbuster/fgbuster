e2dcb7ed11debfd46f2773d2961a9e8ba62990a5
dpole <davide.pole@gmail.com>
2018-02-27 14:11:14 +0100

    Package structure
    - setup.py file
    - requirements
    - files are in a dustbuster folder
    - example folder
    - test folder (almosty empty)

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "dustbuster",
+    version = "0.0.1",
+    author = "Davide Poletti",
+    author_email = "davide.pole@gmail.com",
+    description = ("Handy parametric component separation tools"),
+    license = "MIT",
+    keywords = "statistics cosmology cmb foregrounds",
+    url = "https://github.com/dpole/xForecast",
+    packages = ['dustbuster'],
+    long_description = read('README.md'),
+    classifiers = ["Development Status :: 1 - Pre-Alpha"],
+    test_suite = "dustbuster.test"
+)
