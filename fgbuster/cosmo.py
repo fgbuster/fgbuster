437a2241ffe7226e74a205c1b7a32f23dd187121
Josquin Errard <josquin.errard@gmail.com>
2018-12-04 17:10:34 +0100

    modeling of the stat res

--- fgbuster/cosmo.py
+++ fgbuster/cosmo.py
@@ -7,13 +7,15 @@ import os.path as op
 from fgbuster.xForecast import _get_Cl_cmb
 import sys
 import scipy
-from fgbuster.algebra import W_dB, _mmm
+from fgbuster.algebra import W_dB, _mmm, _indexed_matrix
+import pylab as pl
+
 
 CMB_CL_FILE = op.join(
      op.dirname(__file__), 'templates/Cls_Planck2018_%s.fits')
 
 
-def from_Cl_to_r_estimate(ClBB_tot, ell_v, fsky, ClBB_model_other_than_prim, r_v, **minimize_kwargs):
+def from_Cl_to_r_estimate(ClBB_tot, ell_v, fsky, Cl_BB_prim, ClBB_model_other_than_prim, r_v, **minimize_kwargs):
     """Simple cosmological likelihood
 
     Parameters
@@ -28,6 +30,10 @@ def from_Cl_to_r_estimate(ClBB_tot, ell_v, fsky, ClBB_model_other_than_prim, r_v
     fsky: float
          fraction of the sky to be considered in the cosmological likelihood, used
          to compute the number of degrees of freedom
+    Cl_BB_prim: 1-d array
+         Vector of angular powers, of the same size as `ClBB_tot`.
+         It corresponds to the templates of primordial B-modes
+         /!\ COMPUTED FOR A TENSOR-TO-SCALAR RATIO OF 1
     ClBB_model_other_than_prim: ndarray
          Vector of angular powers, of the same size as `ClBB_tot`.
          The modeled covariance being written as `ClBB_tot_modeled = ClBB_prim(r) + ClBB_model_other_than_prim`
@@ -62,9 +68,8 @@ def from_Cl_to_r_estimate(ClBB_tot, ell_v, fsky, ClBB_model_other_than_prim, r_v
         -2logL = sum_ell [ (2l+1)fsky * ( log(C) + C^-1.D  ) ]
             cf. eg. Tegmark 1998
         '''    
-        Cov_model = np.diag( Cl_BB_prim*r_loc + ClBB_model_other_than_prim)
+        Cov_model = Cl_BB_prim*r_loc + ClBB_model_other_than_prim
         logL = np.sum( (2*ell_v+1)*fsky*( np.log( Cov_model ) + ClBB_tot/Cov_model ))
-
         return logL
     
     # gridding -2log(L)
@@ -89,7 +94,7 @@ def from_Cl_to_r_estimate(ClBB_tot, ell_v, fsky, ClBB_model_other_than_prim, r_v
     return r_fit, sigma_r_fit, likelihood_on_r
 
 
-def estimation_of_Cl_stat_res(Sigma, d_fgs, A_ev, A_dB_ev, comp_of_dB, beta_maxL, invN, lmin, lmax, i_cmb=0):
+def estimation_of_Cl_stat_res(Sigma, d_fgs, A_ev, A_dB_ev, comp_of_dB, beta_maxL, invN, lmin, lmax, i_cmb=0, patch_ids=[]):
 
     """Estimation of the statistical residuals
     following Errard and Stompor 2018.
@@ -139,40 +144,64 @@ def estimation_of_Cl_stat_res(Sigma, d_fgs, A_ev, A_dB_ev, comp_of_dB, beta_maxL
     # first, we preprocess the frequency sky maps
     n_stokes = d_fgs.shape[1]
     n_freqs = d_fgs.shape[0]
-    if isinstance(Sigma, (list,)):
-        n_patches = len(Sigma)
+    print type(patch_ids)
+    if any(patch_ids):
+        n_patches = patch_ids.max()
+    # if isinstance(Sigma, (list,)):
+        # n_patches = len(Sigma)
     else: n_patches = 1
-    
+
+    print 'n_stokes = ', n_stokes
+    print 'n_patches = ', n_patches
+    print 'Sigma = ', Sigma
+
     if n_stokes == 3:  
         d_spectra = d_fgs
     else:  # Only P is provided, add T for map2alm
         d_spectra = np.zeros((n_freqs, 3, d_fgs.shape[2]), dtype=d_fgs.dtype)
         d_spectra[:, 1:] = d_fgs
-    # masked data and fsky
-    mask = d_fgs[0, 0, :] != 0.
-    fsky = mask.astype(float).sum() / mask.size
-    # go to Fourier space and build matrix with
-    # all the observed auto- and cross-spectra 
-    almBs = [hp.map2alm(freq_map, lmax=lmax, iter=10)[2] for freq_map in d_spectra]
-    Cl_fgs = np.zeros((n_freqs, n_freqs, lmax+1), dtype=d_fgs.dtype)
-    for f1 in range(n_freqs):
-        for f2 in range(n_freqs):
-            if f1 > f2:
-                Cl_fgs[f1, f2] = Cl_fgs[f2, f1]
-            else:
-                Cl_fgs[f1, f2] = hp.alm2cl(almBs[f1], almBs[f2], lmax=lmax)
-    # concatanating everything, and correcting for fsky
-    Cl_fgs = Cl_fgs[..., lmin:] / fsky
-    # from the mixing matrix and its derivative at the peak
-    # of the likelihood, we build dW/dB
-    A_maxL = A_ev(beta_maxL)
-    A_dB_maxL = A_dB_ev(beta_maxL)
-    W_dB_maxL = W_dB(A_maxL, A_dB_maxL, comp_of_dB, invN=invN)[:, i_cmb]
-    # and then Cl_YY, cf Stompor et al 2016
-    Cl_YY = _mmm(W_dB_maxL, Cl_fgs.T, W_dB_maxL.T)  
-    # and finally, using the covariance of error bars on spectral indices
-    # we compute the model for the statistical foregrounds residuals, 
-    # cf. Errard et al 2018
-    tr_SigmaYY = np.einsum('ij, lji -> l', Sigma, Cl_YY)
+    d_spectra = d_spectra.T
 
-    return tr_SigmaYY
+    for i_patch in range(n_patches):
+        print 'patch # ', i_patch
+        # we define the sky mask
+        patch_mask = patch_ids == i_patch
+        if not np.any(patch_mask):
+            return None
+        patch_d = d_spectra*0.0
+        patch_d[patch_mask] = d_spectra[patch_mask]
+        # masked data and fsky
+        mask = patch_d[:,1,0] != 0.
+        fsky = mask.astype(float).sum() / mask.size
+        # go to Fourier space and build matrix with
+        # all the observed auto- and cross-spectra
+        almBs = [hp.map2alm(freq_map, lmax=lmax, iter=10)[2] for freq_map in patch_d.T]
+        Cl_fgs = np.zeros((n_freqs, n_freqs, lmax+1), dtype=patch_d.dtype)
+        for f1 in range(n_freqs):
+            for f2 in range(n_freqs):
+                if f1 > f2:
+                    Cl_fgs[f1, f2] = Cl_fgs[f2, f1]
+                else:
+                    Cl_fgs[f1, f2] = hp.alm2cl(almBs[f1], almBs[f2], lmax=lmax)
+        # concatanating everything, and correcting for fsky
+        Cl_fgs = Cl_fgs[..., lmin:] #/ fsky
+        # from the mixing matrix and its derivative at the peak
+        # of the likelihood, we build dW/dB
+        A_maxL = A_ev(beta_maxL[:,i_patch])
+        A_dB_maxL = A_dB_ev(beta_maxL[:,i_patch])
+        # patch_invN = _indexed_matrix(invN, d_spectra.T.shape, patch_mask)
+        W_dB_maxL = W_dB(A_maxL, A_dB_maxL, comp_of_dB, invN=None)[:, i_cmb]
+        # and then Cl_YY, cf Stompor et al 2016
+        Cl_YY = _mmm(W_dB_maxL, Cl_fgs.T, W_dB_maxL.T)  
+        # and finally, using the covariance of error bars on spectral indices
+        # we compute the model for the statistical foregrounds residuals, 
+        # cf. Errard et al 2018
+        tr_SigmaYY = np.einsum('ij, lji -> l', Sigma[...,i_patch], Cl_YY)
+        if i_patch == 0 :
+            Cl_stat_res_model = tr_SigmaYY
+        else: 
+            Cl_stat_res_model += tr_SigmaYY
+
+    return Cl_stat_res_model
+
+
