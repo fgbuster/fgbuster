b2867127cc231aa1c9cb28fbca038e71bf06b3bc
dpole <davide.pole@gmail.com>
2019-01-11 07:18:41 +0100

    pysm_helper renamed and polished

--- fgbuster/observation_helpers.py
+++ fgbuster/observation_helpers.py
@@ -33,6 +33,8 @@ def get_sky(nside, tag='c1d0s0'):
 
     Parameters
     ----------
+    nside: int
+        healpix nside of the sky templates
     tag: string
         See `pysm.nominal
         <https://github.com/bthorne93/PySM_public/blob/master/pysm/nominal.py>`_ 
@@ -43,7 +45,7 @@ def get_sky(nside, tag='c1d0s0'):
     Returns
     -------
     sky: dict
-        Configuration for a PySM.Sky
+        Configuration dictionary. It can be used for constructing a ``PySM.Sky``
     """
     comp_names = {
         'a': 'ame',
@@ -59,8 +61,8 @@ def get_sky(nside, tag='c1d0s0'):
     return sky_config
 
 
-def get_instrument(nside, tag, units='uK_CMB'):
-    """ Get pre-defined instrumental configurations
+def get_instrument(tag, nside=None, units='uK_CMB'):
+    """ Get a pre-defined instrumental configuration
 
     Parameters
     ----------
@@ -69,10 +71,12 @@ def get_instrument(nside, tag, units='uK_CMB'):
         set tag to something random to have a list of the available
         configurations. It can contain the name of multiple experiments
         separated by a space.
+    nside: int
+        If you plan to build 
 
     Returns
     -------
-    sky: dict
+    instr: dict
         It contains the experimetnal configuration of the desired instrument.
         It can be used to construct a ``pysm.Instrument`` or as the
         ``instrument`` argument for, e.g., :func:`basic_comp_sep` and
@@ -84,7 +88,7 @@ def get_instrument(nside, tag, units='uK_CMB'):
         try:
             instrument = getattr(module, '_dict_instrument_'+t)(nside, units)
         except AttributeError:
-            raise ValueError('Instrument %s not available. Chose between: %s.'
+            raise ValueError('Instrument %s not available. Choose between: %s.'
                              % (t, ', '.join(_get_available_instruments())))
         else:
             instruments.append(instrument)
@@ -105,7 +109,8 @@ def _get_available_instruments():
     return [fun.replace(prefix, '') for fun in dir(module) if has_prefix(fun)]
 
 
-def _dict_instrument_test(nside, units='uK_CMB'):
+def _dict_instrument_test(nside=None, units='uK_CMB'):
+    # Mock instrument configuration for testing purposes
     return {
         'frequencies': np.arange(10., 300, 30.),
         'sens_I': (np.linspace(20, 40, 10) - 30)**2,
@@ -122,12 +127,13 @@ def _dict_instrument_test(nside, units='uK_CMB'):
     }
 
 
-def _dict_instrument_planck_P(nside, units='uK_CMB'):
+def _dict_instrument_planck_P(nside=None, units='uK_CMB'):
+    # Planck 2015 results X, A&A, Volume 594, id.A10, 63 pp.
     return {
         'frequencies': np.array([30.0, 44.0, 70.0, 100.0, 143.0, 217.0, 353.0]),
-        'sens_I': np.array([7.5, 7.5, 4.8, 1.3, 1.1, 1.6, 6.9]) * 40.0, # XXX
+        'sens_I': np.array([2.8, 3.0, 4.0, 0.85, 0.9, 1.8, 4]) * 60,
         'sens_P': np.array([7.5, 7.5, 4.8, 1.3, 1.1, 1.6, 6.9]) * 40.0,
-        'beams': np.array([33.16, 28.09, 13.08, 9.66, 7.27, 5.01, 4.86]),
+        'beams': np.array([32.4, 27.1, 13.3, 9.5, 7.2, 5.0, 4.9]),
         'nside': nside,
         'add_noise': True,
         'noise_seed': 1234,
@@ -139,7 +145,8 @@ def _dict_instrument_planck_P(nside, units='uK_CMB'):
     }
 
 
-def _dict_instrument_litebird(nside, units='uK_CMB'):
+def _dict_instrument_litebird(nside=None, units='uK_CMB'):
+    # Matsumura et al., Journal of Low Temperature Physics 184, 824 (2016)
     return {
         'frequencies': np.array([40.0, 50.0, 60.0, 68.4, 78.0, 88.5, 100.0, 118.9, 140.0, 166.0, 195.0, 234.9, 280.0, 337.4, 402.1]),
         'sens_I': np.array([36.1, 19.6, 20.2, 11.3, 10.3, 8.4, 7.0, 5.8, 4.7, 7.0, 5.8, 8.0, 9.1, 11.4, 19.6]) / 1.41,
@@ -156,7 +163,8 @@ def _dict_instrument_litebird(nside, units='uK_CMB'):
     }
 
 
-def _dict_instrument_pico(nside, units='uK_CMB'):
+def _dict_instrument_pico(nside=None, units='uK_CMB'):
+    # https://sites.google.com/umn.edu/picomission/home
     return {
         'frequencies': np.array([21.0, 25.0, 30.0, 36.0, 43.2, 51.8, 62.2, 74.6, 89.6, 107.5, 129.0, 154.8, 185.8, 222.9, 267.5, 321.0, 385.2, 462.2, 554.7, 665.6, 798.7]),
         'sens_I': np.array([16.9, 11.8, 8.1, 5.7, 5.8, 4.1, 3.8, 2.9, 2.0, 1.6, 1.6, 1.3, 2.6, 3.0, 2.1, 2.9, 3.5, 7.4, 34.6, 143.7, 896.4]) / 1.41,
@@ -173,8 +181,8 @@ def _dict_instrument_pico(nside, units='uK_CMB'):
     }
 
 
-def _dict_instrument_cmbs4(nside, units='uK_CMB'):
-    # specifications taken from https://cmb-s4.org/wiki/index.php/Survey_Performance_Expectations
+def _dict_instrument_cmbs4(nside=None, units='uK_CMB'):
+    # https://cmb-s4.org/wiki/index.php/Survey_Performance_Expectations
     return {
         'frequencies': np.array([20, 30, 40, 85, 95, 145, 155, 220, 270]),
         'sens_I': np.array([16.66, 10.62, 10.07, 2.01, 1.59, 4.53, 4.53, 11.61, 15.84]),
@@ -191,7 +199,7 @@ def _dict_instrument_cmbs4(nside, units='uK_CMB'):
     }
 
 
-def _dict_instrument_quijote_mfi(nside, units='uK_CMB'):
+def _dict_instrument_quijote_mfi(nside=None, units='uK_CMB'):
     return {
         'frequencies': np.array([11.0, 13.0, 17.0, 19.0]),
         'sens_I': np.array([2100, 2100, 2100, 2100]) / 1.41,
