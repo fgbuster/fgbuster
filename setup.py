a98030bf0245b6f56e30ae812c5a1bc52215072e
BaptisteJost <jost.baptiste@gmail.com>
2025-01-29 13:28:43 +0900

    updating requirements on scipy. Now aligned with master

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'parameterized',
         'corner',
         'numdifftools',
-        'sympy',
+        'sympy=1.11',
         'healpy',
         'setuptools_git',
         'pysm3',
